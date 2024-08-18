feac49e63f462733e1dea4137106b3315b166ed1
AndrejKovacik57 <andrejkovacik57@gmail.com>
2024-08-18 17:07:06 +0200

    ...

--- Metin2-BOT.py
+++ Metin2-BOT.py
@@ -237,15 +237,15 @@ class ApplicationWindow:
             x_middle = (x2 - x1) // 2
             y_middle = (y2 - y1) // 2
 
-            if self.metin.skills_time == 0:
-                print('skill1')
-                self.metin.skills_time = time.time()
-                self.metin.activate_skills()
-            else:
-                skill_time = time.time() - self.metin.skills_time
-                if skill_time > 1860:
-                    print('skill2')
-                    self.metin.activate_skills()
+            # if self.metin.skills_time == 0:
+            #     print('skill1')
+            #     self.metin.skills_time = time.time()
+            #     self.metin.activate_skills()
+            # else:
+            #     skill_time = time.time() - self.metin.skills_time
+            #     if skill_time > 1860:
+            #         print('skill2')
+            #         self.metin.activate_skills()
             if self.metin.god_buff_cd == 0:
                 print('god buff1')
                 self.metin.god_buff_cd = time.time()
@@ -440,14 +440,17 @@ class Metin:
     def activate_skills(self):
         x1, y1 = 568, 1020  # z lava, z hore
         x2, y2 = 835, 1050  # z prava, z dola
-        max_attempts = 10
         for skill_to_activate in self.skills_to_activate:
-            for counter in range(max_attempts):
+            skill_activated = False
+            counter = 0
+            while not skill_activated:
                 print(f'---{skill_to_activate} try {counter}---')
                 skill_pos = self.skill_positions[skill_to_activate]
                 pixels = []
-                offset = 16 if skill_pos < 5 else 30 # 14 pixels between nums and Fs
-                skill_pixel_position = 32 * (skill_pos - 1) + offset
+                if skill_pos < 5:
+                    skill_pixel_position = 32 * (skill_pos - 1) + 16
+                else:
+                    skill_pixel_position = 32 * (skill_pos - 1) + 30  # 14 pixels between nums and Fs
                 for _ in range(3):
                     np_image = np.array(get_window_screenshot(self.metin_window))
                     skills = np_image[y1: y2, x1: x2]
@@ -459,7 +462,7 @@ class Metin:
                 print(f'num_of_diff_pixels: {num_of_diff_pixels}')
                 if num_of_diff_pixels > 1:
                     print('skill active')
-                    break
+                    skill_activated = True
                 else:
                     if counter > 1:
                         # couldnt activate skill because character is on horse, we go down from mount
