4aa240c4a3b362d830f66f7596dcfeee75c8c61d
AndrejKovacik57 <andrejkovacik57@gmail.com>
2024-08-18 23:42:33 +0200

    fix image mask, changed delays

--- Metin2-BOT.py
+++ Metin2-BOT.py
@@ -333,8 +333,6 @@ class ApplicationWindow:
                 self.metin.metin_window.activate()
                 pydirectinput.press('q')
                 print("No valid contour found.")
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
 
     def bot_solver(self):
         window_title = "EmtGen"
@@ -410,13 +408,13 @@ class Metin:
             for contour in contours:
                 if self.contour_high > cv2.contourArea(contour) > self.contour_low:  # 900
                     x, y, w, h = cv2.boundingRect(contour)
-                    cv2.rectangle(np_image_masked, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw rectangle
+                    cv2.rectangle(np_image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw rectangle
 
                     contour_center_x = x + w // 2
                     contour_center_y = y + h // 2
 
                     # Draw a line from the middle of the screenshot to the center of the contour
-                    cv2.line(np_image_masked, (x_middle, y_middle), (contour_center_x, contour_center_y), (255, 190, 200), 2)
+                    cv2.line(np_image, (x_middle, y_middle), (contour_center_x, contour_center_y), (255, 190, 200), 2)
                     # Optionally, you can calculate the distance between the middle of the screenshot and the contour center
                     cur_distance = abs(x_middle - contour_center_x) + abs(y_middle - contour_center_y)
                     if cur_distance < min_distance:
@@ -426,12 +424,11 @@ class Metin:
         if selected_contour is not None:
             x, y, w, h = cv2.boundingRect(selected_contour)
             selected_contour_pos = (x + w / 2, y + h / 2)
-            cv2.rectangle(np_image_masked, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Draw rectangle
+            cv2.rectangle(np_image, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Draw rectangle
 
-        cv2.imshow('image', np_image_masked)
         if selected_contour_pos is None:
             return None
-        return selected_contour_pos, np_image_masked
+        return selected_contour_pos, np_image
 
     def look_for_bot_check(self, image):
         np_image = np.array(image)
