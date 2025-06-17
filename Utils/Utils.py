eff34cead5320187b09f67b85006d8b3318ae44d
andrej-notebook <andrejkovacik57@gmail.com>
2025-06-17 17:56:14 +0200

    zmena cakana kliku

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -85,7 +85,7 @@ def mouse_left_click(pos_x, pos_y, window_title :str) -> None:
     if active_window and window_title in active_window.title:
         pyautogui.moveTo(pos_x, pos_y)
         print('KLIK KLIK KLIK')
-        time.sleep(2)
+        time.sleep(0.3)
         pyautogui.click()
 
 
